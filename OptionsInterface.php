18071d0867e6ba4956015ead07c1da963e250ddc
Rémy Perona <remperona@gmail.com>
2020-02-06 11:59:27 -0500

    Add interface to define mandatory methods

---
+++ OptionsInterface.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace WPMedia\Options;
+
+/**
+ * Define mandatory methods to implement when using this package
+ */
+interface OptionsInterface {
+	/**
+	 * Gets the option name used to store the option in the WordPress database.
+	 *
+	 * @param string $name Unprefixed name of the option.
+	 *
+	 * @return string Option name used to store it
+	 */
+	public function getOptionName( $name);
+
+	/**
+	 * Gets the option for the given name. Returns the default value if the value does not exist.
+	 *
+	 * @author Remy Perona
+	 *
+	 * @param string $name   Name of the option to get.
+	 * @param mixed  $default Default value to return if the value does not exist.
+	 *
+	 * @return mixed
+	 */
+	public function get( $name, $default = null );
+
+	/**
+	 * Sets the value of an option. Update the value if the option for the given name already exists.
+	 *
+	 * @author Remy Perona
+	 * @param string $name Name of the option to set.
+	 * @param mixed  $value Value to set for the option.
+	 *
+	 * @return void
+	 */
+	public function set( $name, $value );
+
+	/**
+	 * Deletes the option with the given name.
+	 *
+	 * @author Remy Perona
+	 *
+	 * @param string $name Name of the option to delete.
+	 *
+	 * @return void
+	 */
+	public function delete( $name );
+
+	/**
+	 * Checks if the option with the given name exists.
+	 *
+	 * @author Remy Perona
+	 *
+	 * @param string $name Name of the option to check.
+	 *
+	 * @return boolean True if the option exists, false otherwise
+	 */
+	public function has( $name );
+}
