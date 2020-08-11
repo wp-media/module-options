02290d27d572c3758b2d5207268aede559475166
Rémy Perona <remperona@gmail.com>
2020-08-11 17:06:02 -0400

    remove unnecessary files

--- Tests/Unit/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace WPMedia\Options\Tests\Unit;
-
-use function WPMedia\Options\Tests\init_test_suite;
-
-require_once dirname( dirname( __FILE__ ) ) . '/bootstrap-functions.php';
-init_test_suite( 'Unit' );
